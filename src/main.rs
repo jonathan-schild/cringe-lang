a26faeb6b2debc7750a84b1a934d56dd48742eb3
Jonathan "Nath" Schild <nath@j-schild.de>
2025-03-09 13:00:07 +0100

    Add thiserror

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,12 @@
 // #![warn(clippy::missing_docs_in_private_items)]
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
 
-use build_info::build_info;
+use std::io;
+
 use log::info;
+use thiserror::Error;
+
+use build_info::build_info;
 
 mod build_info;
 mod lexer;
@@ -17,4 +21,10 @@ mod tokens;
 pub fn main() {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     info!("{}", build_info());
+}
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error(transparent)]
+    Io(#[from] io::Error),
 }
