0056393d99cbf74a8d05122ec3f85bb60b3f1f2e
Jonathan "Nath" Schild <nath@j-schild.de>
2025-03-09 11:27:25 +0100

    More clippy \o/

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
  * Copyright (c) 2025 Jonathan "Nath" Schild. Licensed under the EUPL-1.2
  */
 
+#![warn(clippy::cargo)]
+#![warn(clippy::pedantic)]
+// #![warn(clippy::missing_docs_in_private_items)]
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
 
 use build_info::build_info;
